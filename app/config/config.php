4d1c3930f777c2bea35c7567ebdae80dc4d4dd0a
nperon <nperon@gmail.com>
2020-01-04 20:24:42 +0100

    section 5 user authentication completed

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,6 @@
 <?php
     // DB params
-    define('DB_HOST', '192.168.64.4');
+    define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', '123456');
     define('DB_NAME', 'shareposts');
